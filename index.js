c0635f27e6ab9ffa117d5064283023353c9a502b
sd19671 <scott.dugan@hotmail.com>
2023-09-26 16:28:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import GitHubCollector from './collector.js/'
 
 // https://github.com/microsoft
-const collector = new GitHubCollector('https://api.github.com', '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', 'microsoft'); 
+const collector = new GitHubCollector('https://api.github.com', '<your pat>', 'microsoft'); 
 
 collector.iterateRepositories();
 
